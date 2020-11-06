399ef2fe3a4881c30260a6714d2abbd8786e708c
TigranMargaryan <margaryantigran4@gmail.com>
2020-11-07 00:27:44 +0400

    updated code

--- help-yourself.web/src/main/java/com/help/yourself/web/Controller/MessageController.java
+++ help-yourself.web/src/main/java/com/help/yourself/web/Controller/MessageController.java
@@ -4,7 +4,14 @@ import com.help.yourself.core.data.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class MessageController {
@@ -15,4 +22,50 @@ public class MessageController {
 
         return chatMessage;
     }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/publicChatRoom")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        System.out.println(chatMessage.getContent());
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @RequestMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        String username = (String) request.getSession().getAttribute("username");
+
+        if (username == null || username.isEmpty()) {
+            return "redirect:/login";
+        }
+        model.addAttribute("username", username);
+
+        return "chat";
+    }
+
+    @RequestMapping(path = "/logins", method = RequestMethod.GET)
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @RequestMapping(path = "/logins", method = RequestMethod.POST)
+    public String doLogin(HttpServletRequest request, @RequestParam(defaultValue = "") String username) {
+        username = username.trim();
+
+        if (username.isEmpty()) {
+            return "login";
+        }
+        request.getSession().setAttribute("username", username);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession(true).invalidate();
+
+        return "redirect:/login";
+    }
+
 }
